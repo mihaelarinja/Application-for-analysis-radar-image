1036c862d07a66d534fb2a3fd20c59f403b01a39
mihaela.rinja <mihaela.rinja@mta.ro>
2022-01-05 13:20:26 +0200

    Create project and make setup with cluster

--- licenta/main.py
+++ licenta/main.py
@@ -9,9 +9,6 @@ def print_hi(name):
     print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
